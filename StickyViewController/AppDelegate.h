156f2c505ccfd1afb3684727bf8d1adb613ecdf7
Jesus Morales <jesusam@umich.edu>
2013-07-12 09:52:22 -0400

    Initial Commit

---
+++ StickyViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StickyViewController
+//
+//  Created by Jesus Morales on 7/12/13.
+//  Copyright (c) 2013 Jesus Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StickyViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StickyViewController
+//
+//  Created by Jesus Morales on 7/12/13.
+//  Copyright (c) 2013 Jesus Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
